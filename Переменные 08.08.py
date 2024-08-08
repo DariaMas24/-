116ac48703fab03a54b1dd5f82193091863b358e
DariaMas24 <dariamaslova2016@yandex.ru>
2024-08-08 23:41:48 +0300

    Add files via upload

---
+++ Переменные 08.08.py
@@ -0,0 +1,9 @@
+Number_of_completed_homework = "12"
+print(Number_of_completed_homework)
+The_number_of_hours_spent = "1.5"
+print(The_number_of_hours_spent)
+Name_of_the_course_of_study = "Python"
+print(Name_of_the_course_of_study)
+time = "Время на одно задание, часов"
+print(time)
+print(1.5 / 12)
