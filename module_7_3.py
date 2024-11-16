45b470f854dd82326bfec476f150428e90d768cf
DariaMas24 <dariamaslova2016@yandex.ru>
2024-11-16 16:35:31 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -1,48 +1,38 @@
+import io
+from pprint import pprint
+print()
 class WordsFinder:
-    def __init__(self, *file_name):
-        self.file_names = [*file_name]
-        self.file_name = file_name
-
+    def __init__(self, *file_names):
+        self.file_names = file_names
     def get_all_words(self):
         all_words = {}
-        words = []
-        str_punctuation = [',', '.', '=', '!', '?', ';', ':', ' - ']
         for file_name in self.file_names:
-            with open(file_name, 'r', encoding='utf-8') as opener:
-                for line in opener:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = []
+                for line in file:
                     line = line.lower()
-                    for p in str_punctuation:
-                        if p in line:
-                            line = line.replace(p, ' ')
-                    split_line = line.split(sep=' ')
-                    words.append(split_line)
-        sorted_list = [x for y in words for x in y]
-        all_words[self.file_name] = sorted_list
+                    punctuation = [',', '.', '=', '!', '?', ';', ':']
+                    for punct in punctuation:
+                        line = line.replace(punct, '')
+                    line = line.replace(' - ', ' ')
+                    words.extend(line.split())
+                all_words[file_name] = words
         return all_words
-
     def find(self, word):
-        dict_ = self.get_all_words()
-        list_ = []
-        for name, words in dict_.items():
-            for w in words:
-                if word.lower() in w:
-                    index = words.index(w)
-                    list_.append(self.file_name)
-                    list_.append(index+1)
-                    break
-        return list_
+        places = {}
+        for key, value in self.get_all_words().items():
+            if word.lower() in value:
+                places[key] = value.index(word.lower()) + 1
+        return places
 
     def count(self, word):
-        dict_ = self.get_all_words()
-        list_ = []
-        count = 0
-        for name, words in dict_.items():
-            for w in words:
-                if word.lower() in w:
-                    count += 1
-        list_.append(self.file_name)
-        list_.append(count)
-        return list_
+        counters = {}
+        for value, key in self.get_all_words().items():
+            words_count = key.count(word.lower())
+            counters[value] = words_count
+
+        return counters
+
 
 
 finder2 = WordsFinder('test_file.txt')
