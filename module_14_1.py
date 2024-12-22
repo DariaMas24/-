dacab1981e8db70163b2529a0a9ef107bd81ab9c
DariaMas24 <dariamaslova2016@yandex.ru>
2024-12-22 19:25:44 +0300

    Update module_14_1.2.py

--- module_14_1.py
+++ module_14_1.py
@@ -1,39 +1,63 @@
 import sqlite3
 
 
-connection = sqlite3.connect("not_telegram.db ")
-cursor = connection.cursor()
+conn = sqlite3.connect('not_telegram.db')
+cursor = conn.cursor()
+
 
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER,
-balance INTEGER 
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
 )
 ''')
 
-#cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
 
-for i in range(1,11):
-    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
-                      (f'User{i + 1}', f'example{i}@gmail.com', f'{i * 10}', 1000))
+users_data = [
+    ('User1', 'example1@gmail.com', 10, 1000),
+    ('User2', 'example2@gmail.com', 20, 1000),
+    ('User3', 'example3@gmail.com', 30, 1000),
+    ('User4', 'example4@gmail.com', 40, 1000),
+    ('User5', 'example5@gmail.com', 50, 1000),
+    ('User6', 'example6@gmail.com', 60, 1000),
+    ('User7', 'example7@gmail.com', 70, 1000),
+    ('User8', 'example8@gmail.com', 80, 1000),
+    ('User9', 'example9@gmail.com', 90, 1000),
+    ('User10', 'example10@gmail.com', 100, 1000)
+]
 
-for i in range(1,11,2):
-    cursor.execute("UPDATE Users SET balance = ? WHERE username = ?", (500, f"User{i}"))
+cursor.executemany('''
+INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)
+''', users_data)
 
-for i in range(1,11,3):
-    cursor.execute("DELETE FROM Users WHERE username = ?", (f"User{i}",))
+# Обновление баланса для каждых двух записей начиная с первой
+cursor.execute('''
+UPDATE Users
+SET balance = 500
+WHERE id % 2 == 1
+''')
 
-cursor.execute("SELECT username, email, age, balance FROM Users WHERE AGE != 60")
+# Удаление каждой третьей записи начиная с первой
+cursor.execute('''
+DELETE FROM Users
+WHERE id % 3 == 1
+''')
 
-users = cursor.fetchall()
-for user in users:
-    username, email, age, balance = user
-    #print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
-    print(f'Имя: {username} | Почта: {email} | Возраст: {age} | Баланс: {balance}')
-    #print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
+# Выборка всех записей, кроме тех, у кого возраст равен 60
+cursor.execute('''
+SELECT username, email, age, balance
+FROM Users
+WHERE age != 60
+''')
 
-connection.commit()
-connection.close()
+# Получаем все результаты и выводим их в нужном формате
+results = cursor.fetchall()
+for row in results:
+    print(f'Имя: {row[0]} | Почта: {row[1]} | Возраст: {row[2]} | Баланс: {row[3]}')
+
+# Сохраняем изменения и закрываем соединение
+conn.commit()
+conn.close()
